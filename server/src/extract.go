9b4f84ab2400787040e756040903eba952b9fa09
rocstack <bradhayes000@gmail.com>
2023-06-17 18:28:14 +0100

    Latest

--- server/src/extract.go
+++ server/src/extract.go
@@ -19,15 +19,15 @@ func extractMetadata(files []string) {
 	// Extract metadata from each file
 	for _, file := range files {
 		// artist, album, song := extractMetadataFile(file)
-		extractMetadataFile(file)
+		extractFile(file)
 		// fmt.Printf("Artist: %s, Album: %s, Song: %s\n", artist, album, song)
 	}
 }
 
-// extractMetadata extracts the artist name, album name, and song name from the given file path.
-func extractMetadataFile(file string) (string, string, string) {
+// extracts the file paths
+func extractFile(file string) (string, []string) {
 	// Get the file name without extension
-	name := strings.TrimSuffix(filepath.Base(file), filepath.Ext(file))
+	fileName := strings.TrimSuffix(filepath.Base(file), filepath.Ext(file))
 
 	// Split the file path into parts
 	parts := strings.Split(filepath.Dir(file), string(filepath.Separator))
@@ -37,31 +37,7 @@ func extractMetadataFile(file string) (string, string, string) {
 		parts = parts[2:]
 	}
 
-	fmt.Printf(parts[0] + "\n")
+	fmt.Print(parts, fileName)
 
-	// Extract artist, album, and song names
-	var artist, album, song string
-	switch len(parts) {
-	case 1:
-		// Song file is in the root of the directory
-		artist = "Unknown Artist"
-		album = "Unknown Album"
-		song = name
-	case 2:
-		// Song file is under the artist directory
-		artist = parts[0]
-		album = "Unknown Album"
-		song = name
-	case 3:
-		// Song file is under the album directory
-		artist = parts[0]
-		album = parts[1]
-		song = name
-	default:
-		// Invalid file path
-		artist = "Unknown Artist"
-		album = "Unknown Album"
-		song = name
-	}
-	return artist, album, song
+	return fileName, parts
 }
